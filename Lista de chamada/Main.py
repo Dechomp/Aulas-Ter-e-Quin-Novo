9f1c5d7a59366e7814781f19f7af0733c18ae0f6
Dechomp <112034108+Dechomp@users.noreply.github.com>
2025-08-26 20:19:26 +0000

    Vetores

---
+++ Lista de chamada/Main.py
@@ -0,0 +1,17 @@
+def main():
+    quantAlunos = int(input("Digite a quantidade de alunos: "))
+
+    while quantAlunos < 1:
+        print("Número inválido, digite novamente")
+        quantAlunos = int(input("Digite a quantidade de alunos: "))
+    
+    nomes = [""] * quantAlunos
+
+    i = 0
+
+    while i <  range(len(nomes)):
+        nomes[i] = input("Digite o nome do aluno: ")
+        i += 1
+    print(nomes)
+    return 0
+main()
